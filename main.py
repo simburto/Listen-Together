c4d1457f89168a5d34cc7cb5c1b49d53173d4e18
simburto <tingseamus@gmail.com>
2023-11-02 09:35:24 -0400

    added logic for spotify (playback and hosting)

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ import spotipy.util as util
 from dotenv import load_dotenv
 from os import getenv as env
 
+#functions needed for website
+sphost = False
+trackname = 'Show'
+artistname = 'Ado'
+offset = 100
+
 load_dotenv()
 client_id = env('API_KEY')
 client_secret = env('API_SECRET')
 redirect_uri = 'https://callback.simburrito.repl.co/'
 username = input('username')
-playlist_link = input('playlist link')
 if len(client_id) != 0 and len(client_secret) != 0:
     print(".env values imported")
 else:
@@ -25,3 +30,18 @@ scopes = 'app-remote-control streaming user-modify-playback-state user-read-curr
 token = util.prompt_for_user_token(username, scopes, client_id, client_secret, redirect_uri)
 spu = spotipy.Spotify(auth=token)
 
+def spotify(sphost, trackname, artistname, offset):
+    if sphost == True:
+        track = spu.current_user_playing_track()
+        return track
+    else:
+        search_term = f"{artistname}%20{trackname}" 
+        trackid = []
+        track = spd.search(q=search_term, limit=1, offset = 0, type='track', market='CA')
+        print(type(track))
+        trackid = [track['tracks']['items'][0]['uri']]
+        artistname = track['tracks']['items'][0]['artists'][0]['name']
+        trackname = track['tracks']['items'][0]['name']
+        spu.start_playback(uris=trackid, position_ms = offset)
+        return trackid, artistname, trackname
+print(spotify(sphost, trackname, artistname, offset))
